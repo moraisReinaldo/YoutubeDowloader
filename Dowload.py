622c688da7db0b92e2362df79ae22b0da475c464
Reinaldo Henrique Morais <116378866+RHMestudante@users.noreply.github.com>
2022-10-22 15:16:20 -0300

    Update Dowload.py

--- Dowload.py
+++ Dowload.py
@@ -5,18 +5,19 @@ class Tela:
     def __init__(self):
         l = [
             [sg.Text('link'), sg.Input(key='li')],
-            [sg.Button('Dowload')]
+            [sg.Button('Dowload')],
+            [sg.Output(size=(10,10))]
         ]
         
-        j = sg.Window('Dowloader').layout(l)
-        
-        self.button, self.values = j.Read()
+        self.j = sg.Window('Dowloader').layout(l)
         
     def Iniciar(self):
-        link = self.values['li']
-        yt = pytube.YouTube(link)
-        yt.streams.first().download()
-        print("Baixado", link)
+        while True:
+            self.button, self.values = self.j.Read()
+            link = self.values['li']
+            yt = pytube.YouTube(link)
+            yt.streams.first().download()
+            print("Baixado", link)
 
 tela =  Tela()
 tela.Iniciar()
